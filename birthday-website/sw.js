de8f39ff406f02b17102643261d6ae6b3fbf3842
Susant07-star <bhandarinishan557@gmail.com>
2026-09-08 13:43:00 +0545

    Auto update installed PWAs

--- birthday-website/sw.js
+++ birthday-website/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bday-v2';
+const CACHE = 'bday-v3';
 const ASSETS = [
   '/', '/index.html', '/css/style.css', '/js/messages.js', '/manifest.json',
   '/icons/favicon.svg', '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'
